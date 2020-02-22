6548e30f5f769843b201a1ddf0f3ddd106a90a44
Hendrick Salomon <hsalomon86@gmail.com>
2020-02-22 19:58:06 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,15 +20,15 @@ class Transfer
         self.sender.balance -= amount
         self.receiver.balance += amount
         self.status = "complete"
-        else
-          self.status = "rejected"
-          "Transaction rejected. Please check your account balance."
-        end
       else
         self.status = "rejected"
         "Transaction rejected. Please check your account balance."
       end
+    else
+      self.status = "rejected"
+      "Transaction rejected. Please check your account balance."
     end
+  end
 
     def reverse_transfer
       if self.status == "complete"
